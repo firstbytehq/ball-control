2810d0afbe96e8f9f62881f7c52c52b3c99ac27d
Anju mary paul <anju@firstbytedigital.com>
2020-04-01 18:12:09 +0530

    Ball movement boundary added

--- src/screens/BallControl.js
+++ src/screens/BallControl.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {StyleSheet, Dimensions } from 'react-native';
+import {StyleSheet, Dimensions, StatusBar} from 'react-native';
 import {
   accelerometer,
   setUpdateIntervalForType,
@@ -11,34 +11,30 @@ import Matter from 'matter-js';
 
 import Circle from '../components/Circle';
 
-const {height, width} = Dimensions.get('window');
+const {height, width} = Dimensions.get('screen');
+const windowHeight = Dimensions.get('window').height;
+const navbarHeight = height - windowHeight + StatusBar.currentHeight;
 
 const BALL_SIZE = 20;
 
-const mid_point = width / 2 - BALL_SIZE / 2;
+// const mid_point = width / 2 - BALL_SIZE / 2;
 
 const ballSettings = {
   isStatic: true,
 };
 
-const ball = Matter.Bodies.circle(0, 0, BALL_SIZE, {
+const ball = Matter.Bodies.circle(18, 18, BALL_SIZE, {
   ...ballSettings,
   label: 'ball',
-});
-
-const floor = Matter.Bodies.rectangle(width / 2, height, width, 10, {
-  isStatic: true,
-  isSensor: true,
-  label: 'floor',
 });
 
 setUpdateIntervalForType(SensorTypes.accelerometer, 30); //15
 
 export default class App extends Component {
   state = {
-    x: 0,
-    y: 0,
-    isGameReady: false,
+    x: 18, // the ball's initial X position
+    y: 18, // the ball's initial Y position
+    isGameReady: false, // game is not ready by default
   };
 
   constructor(props) {
@@ -46,16 +42,17 @@ export default class App extends Component {
 
     const {engine, world} = this.addObjectsToWorld(ball);
     this.entities = this.getEntities(engine, world, ball);
-
+    //
     this.physics = (entities, {time}) => {
-      let engine = entities.physics.engine;
-      engine.world.gravity.y = 10; //0.5
-      Matter.Engine.update(engine, time.delta);
+      let engine = entities.physics.engine; // get the reference to the physics engine
+      engine.world.gravity.y = 1; //0.5 // set the gravity of Y axis
+      Matter.Engine.update(engine, time.delta); // move the game forward in time
       return entities;
     };
   }
 
   componentDidMount() {
+    // handle object position
     accelerometer.subscribe(({x, y}) => {
       Matter.Body.setPosition(ball, {
         x: this.state.x + x,
@@ -66,22 +63,24 @@ export default class App extends Component {
         state => ({
           x: x + state.x,
           y: y + state.y,
-        }),
+        }), // to set boundary
         () => {
           if (
-            this.state.x < 0 ||
-            this.state.x > width ||
-            this.state.y < 0 ||
-            this.state.y > height
+            this.state.x < 18 ||
+            // this.state.x > width ||
+            this.state.y < 18 ||
+            // this.state.y > height ||
+            this.state.x > width - 60 ||
+            this.state.y > height - navbarHeight - 60
           ) {
             Matter.Body.setPosition(ball, {
-              x: mid_point,
-              y: mid_point,
+              x: this.state.x - x,
+              y: this.state.y - y,
             });
 
             this.setState({
-              x: mid_point,
-              y: mid_point,
+              x: this.state.x - x,
+              y: this.state.y - y,
             });
           }
         },
@@ -97,20 +96,21 @@ export default class App extends Component {
   //   accelerometer.stop();
   // }
 
+  // add object to world
   addObjectsToWorld = ball => {
     const engine = Matter.Engine.create({enableSleeping: false});
     const world = engine.world;
 
-    let objects = [ball, floor];
+    let objects = [ball];
 
     Matter.World.add(world, objects);
-
     return {
       engine,
       world,
     };
   };
 
+  // to render moving object
   getEntities = (engine, world, ball) => {
     const entities = {
       physics: {
@@ -148,14 +148,8 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    margin: 20,
+    borderWidth: 1,
     backgroundColor: '#F5FCFF',
-  },
-  header: {
-    padding: 20,
-    alignItems: 'center',
-  },
-  scoreText: {
-    fontSize: 25,
-    fontWeight: 'bold',
   },
 });
